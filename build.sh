1c23a11990f6f52d4d2be5cbf1a8c60bd9788888
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2025-04-19 12:06:04 +0000

    Add build command and add py support

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+START_DIR="$(pwd)"
+OUT_DIR="$START_DIR/docs/.vitepress/dist"
+HEADER_PATH="$OUT_DIR/_headers"
+
+rm -rf docs/contents || true
+git clone https://github.com/sounddrill31/course-notes-python docs/contents -b main
+cd docs
+if [[ "$1" == "cf" ]]; then
+    sed -i 's/base.*,//g' .vitepress/config.mts
+fi
+npx vitepress build 
+cd ..
+
+echo "Built from $START_DIR"
+echo "Output will be in $OUT_DIR"
+
+curl https://raw.githubusercontent.com/sounddrill31/course-notes-python/refs/heads/vitepress/headers > "$HEADER_PATH"
+echo "Saved _headers in $HEADER_PATH"
+
+echo "---"
+echo "Displaying $HEADER_PATH"
+cat "$HEADER_PATH"
+echo "---"
--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vitepress'
+import { vitepressPythonEditor } from 'vitepress-python-editor/vite-plugin'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -34,5 +35,10 @@ export default defineConfig({
     //socialLinks: [
     //  { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     //]
+  },
+  vite: {
+    plugins: [
+      vitepressPythonEditor({ assetsDir: 'docs/.vitepress/dist/assets' }),
+    ]
   }
 })
